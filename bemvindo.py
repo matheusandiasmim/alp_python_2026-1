175f1d52cd783a5a7a6ffd7843bdf3e662e7840c
matheusandiasmim <solairejacketraiden@gmail.com>
2026-05-07 11:59:24 -0300

    aula03

--- bemvindo.py
+++ bemvindo.py
@@ -1,1 +1,7 @@
-print('bem vindo')
+
+numero = int(input("Digite um número inteiro positivo: "))
+if numero % 2 == 0 :
+    resultado = numero ** 2
+else: 
+    resultado = numero ** 3
+print("Resultado:", resultado)
---
+++ entrada.py
@@ -0,0 +1,4 @@
+nome = input("digite seu nome: ")
+altura = input("fala sua altura: ")
+idade = input("fala sua idade:  ")
+print("seja bem vindo",nome,"voce tem", idade,"anos e tem", altura," de altura")
