64ac9a72e63654f065c814faa99726ad67102641
matheusandiasmim <solairejacketraiden@gmail.com>
2026-05-08 11:07:36 -0300

    revisaoalt

--- taarefa.py
+++ taarefa.py
@@ -1,4 +1,4 @@
-valor = int(input("digite o valor do produto: "))
+valor = float(input("digite o valor do produto: "))
 quant = int(input("digite a quantidade: "))
 preço = valor * quant
 print(f"a sua compra deu: {preço}")
